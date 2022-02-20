fb5861ed5bd09ba4536ca1237c4fea63639a4ad0
Saurav Sagar <37689450+ss26690@users.noreply.github.com>
2022-02-20 21:57:04 +0530

    Firebase setup (#1)
    * adding app icon and app backgound image
    
    * Firebase crashAnalatics, firestore and Analatics integrated
    
    * deleted google info plist
    
    * update gitignor

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -1,18 +1,27 @@
-//
-//  ViewController.swift
-//  QuizApp
-//
-//  Created by Saurav Sagar on 20/02/22.
-//
-
 import UIKit
+import FirebaseFirestore
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let db = Firestore.firestore()
+        db.collection("items").getDocuments(completion: { (querySnapshort, err) in
+            if let err = err {
+                debugPrint("Error getting document: \(err)")
+            } else {
+                if let documents = querySnapshort?.documents {
+                    for document in  documents {
+                        debugPrint("\(document.documentID) => \(document.data())");
+                    }
+                }
+            }
+        })
+
     }
+    
 
 
 }
